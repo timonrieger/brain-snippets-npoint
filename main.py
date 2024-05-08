f01ebaf137accef471020d2620eb1e991157a30e
Timon Rieger <timon@riegerx.de>
2024-05-08 11:03:40 +0200

    add new favicon + change images + remove contact page

--- main.py
+++ main.py
@@ -143,22 +143,9 @@ def edit_post(npoint_id):
 def about():
     return render_template("about.html")
 
-
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        data = request.form
-        send_email(data["name"], data["email"], data["phone"], data["message"])
-        return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False)
-
-
-def send_email(name, email, phone, message):
-    email_message = f"Subject:Blog Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(EMAIL, PASSWORD)
-        connection.sendmail(EMAIL, EMAIL, email_message)
+@app.route("/npoint")
+def npoint():
+    return redirect("https://www.npoint.io/docs/55ec3c86cd78032d2742")
 
 
 if __name__ == "__main__":
